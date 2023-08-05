8ae815667fe9185d0c03524f0670c172090e52e2
Alexey Sevastynov <alexseva94@gmail.com>
2023-08-05 22:27:30 +0300

    install jsonwebtoken.

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const app = express();
+app.use(express.json());
 const PORT = 4444;
 
 app.get("/", (req, res) => {
   res.send(`Hello world!`);
+});
+
+app.post("/auth/login", (req, res) => {
+  console.log(req.body);
+
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: "Alexey",
+      passwordHash: req.body.passwordHash,
+    },
+    "secret"
+  );
+  res.json({ success: true, token });
 });
 
 app.listen(PORT, (err) => {
