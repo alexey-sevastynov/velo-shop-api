411068ac7b33a633b7e5cbc938a9daf4b2510f4e
Alexey Sevastynov <alexseva94@gmail.com>
2023-08-06 12:16:02 +0300

    added cors + limit express.json

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const { validationResult } = require("express-validator");
 require("dotenv").config();
 
+const cors = require("cors");
+
 const { register, login, getMe } = require("./controllers/UserController");
 
 const handleValidationErrors = require("./utils/handleValidationErrors");
@@ -26,7 +28,10 @@ const {
 } = require("./controllers/PostController");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb" }));
 const PORT = 4444;
 
 mongoose
