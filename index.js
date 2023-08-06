431a1c0913c2dc3eb3ff267db5b4b93897fbfba1
Alexey Sevastynov <alexseva94@gmail.com>
2023-08-06 19:48:13 +0300

    increase limit json

--- index.js
+++ index.js
@@ -29,9 +29,11 @@ const {
 
 const app = express();
 app.use(cors());
+
 app.use(express.json());
-app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ limit: "5mb" }));
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb" }));
+
 const PORT = 4444;
 
 mongoose
