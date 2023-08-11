3e1e222e1fd3eb47a7e69c9dfe97655850f22939
Alexey Sevastynov <alexseva94@gmail.com>
2023-08-11 23:38:46 +0300

    change limit

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ const {
 const app = express();
 app.use(cors());
 
-app.use(express.json({ limit: "25mb" }));
-app.use(express.urlencoded({ limit: "25mb" }));
+app.use(express.json({ limit: "2500kb" }));
 
 const PORT = 4444;
 
